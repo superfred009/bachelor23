1da948fcec696c985d79438c642fe50148a18073
S341546 <70863160+S341546@users.noreply.github.com>
2023-03-31 14:23:33 +0200

    tester å hente inn hele produktlista.

--- apps/svelte/src/routes/store.js
+++ apps/svelte/src/routes/store.js
@@ -10,11 +10,29 @@ export const apiData = writable([]);
 For our use case, we only care about the drink names, not the other information.
 Here, we'll create a derived store to hold the drink names.
 **/
-export const drinkNames = derived(apiData, ($apiData) => {
+export const titleNames = derived(apiData, ($apiData) => {
   // @ts-ignore
   if ($apiData.products){
     // @ts-ignore
-    return $apiData.products.map(product => product.title);
+    return $apiData.products.map(products => products.title);
   }
-  return "feil i drinknames";
+  return "feil i titleNames";
+});
+
+export const description = derived(apiData, ($apiData) => {
+  // @ts-ignore
+  if ($apiData.products){
+    // @ts-ignore
+    return $apiData.products.map(products => products.description);
+  }
+  return "feil i description";
+});
+
+export const images = derived(apiData, ($apiData) => {
+  // @ts-ignore
+  if ($apiData.products){
+    // @ts-ignore
+    return $apiData.products.map(products => products.images);
+  }
+  return "feil i images";
 });
