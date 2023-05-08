67847dff3d97020ee9b0f9eac5a2676ec20ceec6
Aleksanderhoel <s333453@oslomet.no>
2023-05-08 14:52:51 +0200

    Henter inn 2x API i alle rammeverkene

--- apps/svelte/src/routes/utils/products.ts
+++ apps/svelte/src/routes/utils/products.ts
@@ -1,8 +1,11 @@
 import type { Product } from "../types/Product";
 
 export async function load(): Promise<Product[]> {
-  const response = await fetch("https://dummyjson.com/products/");
-  const data = await response.json();
+  const response1 = await fetch("https://dummyjson.com/products/");
+  const data1 = await response1.json();
 
-  return data.products;
+  const response2 = await fetch("https://dummyjson.com/products/");
+  const data2 = await response2.json();
+
+  return [...data1.products, ...data2.products];
 }
