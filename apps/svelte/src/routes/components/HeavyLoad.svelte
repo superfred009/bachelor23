76c2fff3eb05e38544c28bea1459f7fc39061dbe
fredrikneby <s351889@oslomet.no>
2023-05-11 15:34:31 +0200

    fixed import

--- apps/svelte/src/routes/components/HeavyLoad.svelte
+++ apps/svelte/src/routes/components/HeavyLoad.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-    import { heavyLoad } from "./utils/heavyLoad";
+    import { heavyLoad } from "../utils/heavyLoad";
 
     onMount(() => {
         heavyLoad();
