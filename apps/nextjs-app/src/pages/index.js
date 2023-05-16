18a3ad769fc67ff07aef7bb1de3d4b1dd599df60
fredrikneby <s351889@oslomet.no>
2023-05-16 10:59:30 +0200

    removed heavylload and SSR

--- apps/nextjs-app/src/pages/index.js
+++ apps/nextjs-app/src/pages/index.js
@@ -1,11 +1,27 @@
 import { ProductsGrid } from '../components/ProductPage';
 import { heavyLoad } from '@/components/heavyLoad';
-import {useEffect} from 'react'
+import { useEffect, useState } from 'react';
 
-export default function Home({ products }) {
+export default function Home() {
+  // useEffect(async () => {
+  //   // heavyLoad();
+  // }, []);
+
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    heavyLoad();
-  }, []);
+    const getData = async () => {
+      const res1 = await fetch('https://dummyjson.com/products');
+      const data1 = await res1.json();
+
+      const res2 = await fetch('https://dummyjson.com/products');
+      const data2 = await res2.json();
+
+      const combinedProducts = [...data1.products, ...data2.products];
+      setProducts(combinedProducts);
+    };
+    getData();
+  }, [products]);
 
   return (
     <>
@@ -14,18 +30,20 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res1 = await fetch('https://dummyjson.com/products');
-  const data1 = await res1.json();
+// SSR - Server Side Rendering
 
-  const res2 = await fetch('https://dummyjson.com/products');
-  const data2 = await res2.json();
+// export async function getServerSideProps() {
+//   const res1 = await fetch('https://dummyjson.com/products');
+//   const data1 = await res1.json();
 
-  const combinedProducts = [...data1.products, ...data2.products];
+//   const res2 = await fetch('https://dummyjson.com/products');
+//   const data2 = await res2.json();
 
-  return {
-    props: {
-      products: combinedProducts,
-    },
-  };
-}
+//   const combinedProducts = [...data1.products, ...data2.products];
+
+//   return {
+//     props: {
+//       products: combinedProducts,
+//     },
+//   };
+// }
